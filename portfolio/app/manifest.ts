d65029bde58608ceba60f00b9e01108c7eaaed82
Shen Haoming <167516473+Haoming9527@users.noreply.github.com>
2025-12-20 16:00:20 +0800

    Refactor code structure for improved readability and maintainability

--- portfolio/app/manifest.ts
+++ portfolio/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#ffffff',
     icons: [
       {
-        src: '/icon.png',
+        src: '/icon_light.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icon.png',
+        src: '/icon_light.png',
         sizes: '512x512',
         type: 'image/png',
       },
